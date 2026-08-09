cf76a7fe9dc9c09336d15275c7e4e60ec7152c60
dhruv <dharmydv344@gmail.com>
2026-08-09 12:27:49 +0545

    changes in bubble sort

---
+++ JAVA_PROJECT/tempCodeRunnerFile.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class oneD_Array{
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number elements to be inserted: ");
+        int n = scan.nextInt();
+        int []a = new int[n];
+
+        for(int i=0; i<n;i++){
+          System.out.print("Enter elements : ");
+            a[i] = scan.nextInt();
+        }
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+          scan.close();
+
+    }
+}
